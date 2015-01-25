3116a5d57a1d84edfa97a8b0476415bb03f3db9c
KPouianou <kpouianou@gmail.com>
2015-01-25 02:45:40 -0800

    import step UX fixes

--- app/scripts/common/filters.js
+++ app/scripts/common/filters.js
@@ -295,4 +295,10 @@
         };
     });
 
+    app.filter('capitalize', function () {
+        return function (str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        };
+    });
+
 }());
